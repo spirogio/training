49488f554cfc17c77b0d417b036496bb14e2c00c
Spyridon Giochalas <spirogio@hotmail.com>
2023-01-02 11:16:21 -0500

    added new excersizes and chapter 3

---
+++ ComputerSystems/chapter03/Ex_343.c
@@ -0,0 +1,8 @@
+long exchange(long *xp, long y){
+	long x = *xp;
+	*xp = y;
+	return x;
+}
+
+
+
---
+++ ComputerSystems/chapter03/Ex_343.s
@@ -0,0 +1,15 @@
+	.file	"Ex_343.c"
+	.text
+	.globl	exchange
+	.type	exchange, @function
+exchange:
+.LFB0:
+	.cfi_startproc
+	movq	(%rdi), %rax
+	movq	%rsi, (%rdi)
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	exchange, .-exchange
+	.ident	"GCC: (GNU) 4.8.5 20150623 (Red Hat 4.8.5-44)"
+	.section	.note.GNU-stack,"",@progbits
